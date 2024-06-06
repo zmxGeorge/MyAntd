e35daadd3903160f000df6f0b351cdf1e02eb518
周鸣轩 <657427356@qq.com>
2024-06-06 11:01:58 +0800

    更新

---
+++ controls/Template.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement }  from "react";
+import { Form,Table } from "antd";
+
+type AllowedChildren = ReactElement<typeof Form> | ReactElement<typeof Table>;
+
+interface TemplateProps
+{
+   
+}
+
+const Template:React.FC<TemplateProps>=()=>{
+
+}
